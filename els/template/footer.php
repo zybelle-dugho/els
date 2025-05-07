95fe36bb29fb15aedc98d938b32caea68a74c2b3
zybelle-dugho <133933119+zybelle-dugho@users.noreply.github.com>
2025-05-07 20:26:30 +0800

    Delete els directory

--- els/template/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer text-center">
-    All Rights Reserved by Skyrem Brilliant Services. Designed and Developed by <a href="https://www.linkedin.com/in/rasyidialwee">Rasyidi Alwee</a>.
-</footer>
