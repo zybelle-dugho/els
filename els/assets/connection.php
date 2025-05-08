a6b1e59cb4f23e6c8e0766475fba2f5da43924ba
zybelle-dugho <133933119+zybelle-dugho@users.noreply.github.com>
2025-05-08 10:40:33 +0800

    Add files via upload

---
+++ els/assets/connection.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$username = "root";
+$pwd = "";
+$db = "els";
+
+$conn = mysqli_connect($host, $username, $pwd, $db);
+date_default_timezone_set("Asia/Kuala_Lumpur"); 
+
+if(!$conn){
+	echo "<script>alert('Error connecting to database')</script>";
+}
+?>
